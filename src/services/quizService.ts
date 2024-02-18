50340a29970a48f224073262a0a303c917010f89
Kelvin Orhungul <kelvin.orhungul@gmail.com>
2024-02-18 19:30:53 +0100

    Fix missing import issues

--- src/services/quizService.ts
+++ src/services/quizService.ts
@@ -1,12 +1,13 @@
 import { quizRepository } from "api/quizRepository";
+import { QuizCreationData } from "types/quiz";
 
 const quizService = {
-    getAllQuizes: async () => {
-        return await quizRepository.getAllQuizzes();
-    },
-    createQuiz: async (data: QuizCreationData) => {
-        return await quizRepository.createQuiz(data);
-    },
+  getAllQuizes: async () => {
+    return await quizRepository.getAllQuizzes();
+  },
+  createQuiz: async (data: QuizCreationData) => {
+    return await quizRepository.createQuiz(data);
+  },
 };
 
 export default quizService;
