a3eb71b422f8442dabf37352ebd54c8610d3ba11
Kelvin Orhungul <kelvin.orhungul@gmail.com>
2024-02-19 21:30:54 +0100

    Update cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,9 +3,6 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require("./cypress/plugins/index.ts").default(on, config);
-    },
-    baseUrl: "http://localhost:3000",
+    baseUrl: process.env.CYPRESS_BASE_URL || "http://localhost:3000",
   },
 });
